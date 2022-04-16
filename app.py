3264d54c35b2c514691b5a245e0f1b0ec8a2dcf7
Taibah Radwan <90932208+Taibahrad@users.noreply.github.com>
2022-04-16 14:35:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
+ 
+    message = []
+
     # Load current count
     f = open("count.txt", "r")
     count = int(f.read())
@@ -18,8 +21,26 @@ def index():
     f.write(str(count))
     f.close()
 
+    n = open("tempfile.txt", "r")
+    message = n.readlines()
+    n.close()
+    l = 0
+    # print new message content
+    for line in message:
+        l += 1
+        # message.append(line.strip())
+        print("Line{}: {}".format(l, line.strip()))
+
+    # with open("newfile.txt", "a+") as n:
+    #     # Strips the newline character
+    #     for line in Lines:
+    #         l += 1
+    #         print("Line{}: {}".format(l, line.strip()))
+    #         n.write(str(message))
+    #     message = Lines[0]
+
     # Render HTML with count variable
-    return render_template("index.html", count=count)
+    return render_template("index.html", count=count, message=message)
 
 if __name__ == "__main__":
     app.run()
